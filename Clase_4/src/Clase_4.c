e4f47c2e628b05b6ebd3468d7b50c4123fd76d92
alumno <francocampaiola@hotmail.com>
2019-08-29 19:07:21 -0300

    Clase_4

---
+++ Clase_4/src/Clase_4.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : Clase_4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+
+
+int main(void)
+{
+	saludarAlUsuario();
+	return EXIT_SUCCESS;
+}
+
+
---
+++ Clase_4/src/utn.c
@@ -0,0 +1,12 @@
+/*
+ * utn.c
+ *
+ *  Created on: 29 ago. 2019
+ *      Author: alumno
+ */
+
+int saludarAlUsuario(void)
+{
+	printf("HOLA");
+	return 0;
+}
